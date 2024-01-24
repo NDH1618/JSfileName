c059266aaa0c774cb00da8051891fed6cdfca40b
unknown <nguyendoanhong1804@gmail.com>
2024-01-24 11:49:20 +0700

    alpha2

--- jsfile.js
+++ jsfile.js
@@ -1,1 +1,16 @@
-console.log("day la file js")
+console.log("day la file js");
+
+/*
+viết hàm tính số lần xuất hiện của một string a, trong string b
+*/ 
+
+const NewString = (Stringa, Stringb) => {
+    let NewNumber = 0;
+    for(let i = 0; i < Stringb.length; i++){
+        if(Stringa === Stringb[i]){
+            NewNumber += 1;
+        }
+    }
+    console.log("so lan xuat hien cua " + Stringa + " trong" + Stringb + "la " + NewNumber )
+}
+NewString('a', 'laemcolaemlacobevet');
