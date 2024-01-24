f23b59a793672767a1b65bf4fff21cb85b97ae62
unknown <nguyendoanhong1804@gmail.com>
2024-01-24 11:52:45 +0700

    beta2

--- beta/lession2.js
+++ beta/lession2.js
@@ -1,1 +1,11 @@
-console.log("lession2")
+console.log("lession2");
+/*
+hàm nối hai mảng theo thứ tự giảm dần từ đầu vào 2 mảng tăng dần
+*/ 
+const Arrange4 = (array1, array2) => {
+    array1.sort((a, b) => a - b);
+    array2.sort((a, b) => a - b);
+    let c = array1.concat(array2).sort((a, b) => b - a);
+    return c;
+};
+console.log(Arrange4([1, 3, 5, 6], [3, 2, 0 ,9]));
