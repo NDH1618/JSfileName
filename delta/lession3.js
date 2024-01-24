a097e1fb4bf76798ac9858b53168de2827c007b3
unknown <nguyendoanhong1804@gmail.com>
2024-01-24 11:57:01 +0700

    delta2

--- delta/lession3.js
+++ delta/lession3.js
@@ -1,1 +1,8 @@
-console.log("lession3")
+console.log("lession3");
+/*
+hàm random trả về giá trị từ 0 đến 1 có lấy 0 và 1
+*/ 
+const reverseString = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(reverseString(0, 1));
